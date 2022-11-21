88cca4d6ef1daecccab5c81a0620f5eb50bcd5e7
shubhamjaiswal1998 <115568349+shubhamjaiswal1998@users.noreply.github.com>
2022-11-21 15:58:30 +0530

    Add files via upload

---
+++ consonants_count.java
@@ -0,0 +1,42 @@
+public class consonants_count {
+   public static void countCharacter(String str)
+    {
+      
+        int vowels = 0, consonant = 0, specialChar = 0, 
+            digit = 0;
+      
+       
+        for (int i = 0; i < str.length(); i++) {
+              
+            char ch = str.charAt(i);
+      
+            if ( (ch >= 'a' && ch <= 'z') || 
+                (ch >= 'A' && ch <= 'Z') ) {
+      
+               
+                ch = Character.toLowerCase(ch);
+      
+                if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                    ch == 'o' || ch == 'u'){
+                    vowels++;
+                 }
+                else{
+                    consonant++;}
+            }
+            else if (ch >= '0' && ch <= '9'){
+                digit++;   }
+            else {
+                specialChar++;  }
+        }
+          
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonant: " + consonant);
+        System.out.println("Digit: " + digit);
+        System.out.println("Special Character: " + specialChar);
+    }
+    public static void main(String[] args) {
+              String name="inu5ro$ns";
+        countCharacter( name);
+    }
+    
+}
