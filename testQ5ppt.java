c55bb8ae5e1be89009d05285771065c502147d63
shubham jayasval <115568349+shubhamjaiswal1998@users.noreply.github.com>
2023-07-04 16:19:20 +0530

    Add files via upload

---
+++ testQ5ppt.java
@@ -0,0 +1,13 @@
+public class testQ5ppt {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        double height = 1.75;
+        String name = "shubham jaiswal";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+    }
+}
